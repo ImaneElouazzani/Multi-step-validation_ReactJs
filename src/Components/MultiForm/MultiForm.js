319cf95dd767cd4a483a775472d95be40b570ec6
Imane Elouazzani <Imane Elouazzani>
2021-10-26 08:49:47 +0100

    les cartes et le css sont ajoutés

---
+++ src/Components/MultiForm/MultiForm.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import './MultiForm.css'
+import Indicator from './Indicator/Indicator'
+//import CardBegin 
+
+export default function Multiform(){
+
+   const [formIndex, setFormIndex] = useState(1)
+   const [allFormData, setAllFormData] = useState({
+       dietForm: "",
+       foodStyle: [],
+       allergies: [],
+       prefs: {}
+   })
+   
+   return(
+        <div className="container-multiform">
+            <Indicator/>
+        </div>
+    )
+}
